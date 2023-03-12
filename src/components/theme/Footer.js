675bfa1965047fcd14f264e442e0492384f9991a
abdul <abdulrehman.siddiqui@gunsel.com.tr>
2023-03-12 23:34:06 +0200

    first commit

--- src/components/theme/Footer.js
+++ src/components/theme/Footer.js
@@ -8,16 +8,10 @@ const Footer = () => {
   return (
     <AntFooter className="footer">
       <div className="footer-content">
-        <div className="social-icons">
-          <a href="#">
-            <FacebookOutlined />
-          </a>
-          <a href="#">
-            <TwitterOutlined />
-          </a>
-          <a href="#">
-            <InstagramOutlined />
-          </a>
+      <div>
+          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer"><FacebookOutlined style={{ fontSize: '24px', marginRight: '10px' }} /></a>
+          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><TwitterOutlined style={{ fontSize: '24px', marginRight: '10px' }} /></a>
+          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer"><InstagramOutlined style={{ fontSize: '24px' }} /></a>
         </div>
         <div className="footer-bottom">
           Feez Ka Dojo  &copy; {new Date().getFullYear()} All rights reserved.
